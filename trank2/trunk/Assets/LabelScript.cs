cb226954bee7e285379f62129c00a4fc2716f2c9
aandrew <h4crash@mail.ru>
2015-06-08 14:48:02 +0300

    Signed-off-by: aandrew <h4crash@mail.ru>

---
+++ trank2/trunk/Assets/LabelScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LabelScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(LabelShow());
+	}
+
+	protected IEnumerator LabelShow()
+	{
+		yield return new WaitForSeconds(3f);
+		Destroy(gameObject);
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
